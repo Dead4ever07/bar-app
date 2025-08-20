82e35574b1dcc3aff5f354383487aa0f42f17157
luis-santos <luis2004007@gmail.com>
2025-08-20 12:09:21 +0100

    I hope this is the final commit

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  useNavigate,
+} from "react-router-dom";
+import { supabase } from "../../supabaseClient";
 import './Layout.css';
 
 const Navbar = () => {
 
     let user;
-
+    const navigate = useNavigate();
     const handleLogout = async () => {
         const { error } = await supabase.auth.signOut();
 
