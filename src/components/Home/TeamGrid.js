9e5b72bdcf59278207b392bb82283c7329220245
luis-santos <luis2004007@gmail.com>
2025-08-21 18:49:04 +0100

    the grid looks shitty

--- src/components/Home/TeamGrid.js
+++ src/components/Home/TeamGrid.js
@@ -28,7 +28,7 @@ return (
       <div className="equal-height-row w3-row-padding w3-center">
         {members.map((member, index) => (
           <div key={index} className="equal-height-col w3-col l3 m4 s6 w3-margin-bottom">
-            <div className="w3-card w3-round w3-white"  style={{padingTop:"16px"}}>
+            <div className="w3-card w3-round w3-white team-card">
               <div className="w3-container">
                 <div className="team-image-container">
                   <img
